0cd93b27e6817ccc389e98c5ee7234e00044b49f
aollivierre <abdullahollivierre@gmail.com>
2024-09-10 14:46:52 -0700

    added missing functions

--- Install-EnhancedModuleStarterAO.ps1
+++ Install-EnhancedModuleStarterAO.ps1
@@ -115,7 +115,6 @@ function Reset-ModulePaths {
     }
 }
 
-
 function Handle-Error {
     param (
         [Parameter(Mandatory = $true)]
@@ -263,7 +262,65 @@ function Install-ModuleWithPowerShell5Fallback {
     }
 }
 
+function Check-ModuleVersionStatus {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string[]]$ModuleNames
+    )
 
+    #the following modules PowerShellGet and PackageManagement has to be either automatically imported or manually imported into C:\windows\System32\WindowsPowerShell\v1.0\Modules
+
+    Import-Module -Name PowerShellGet -ErrorAction SilentlyContinue
+
+    $results = [System.Collections.Generic.List[PSObject]]::new()  # Initialize a List to hold the results
+
+    foreach ($ModuleName in $ModuleNames) {
+        try {
+
+            Write-EnhancedModuleStarterLog -Message 'Checking module '$ModuleName
+            $installedModule = Get-Module -ListAvailable -Name $ModuleName | Sort-Object Version -Descending | Select-Object -First 1
+            # $installedModule = Check-SystemWideModule -ModuleName 'Pester'
+            $latestModule = Find-Module -Name $ModuleName -ErrorAction SilentlyContinue
+
+            if ($installedModule -and $latestModule) {
+                if ($installedModule.Version -lt $latestModule.Version) {
+                    $results.Add([PSCustomObject]@{
+                        ModuleName = $ModuleName
+                        Status = "Outdated"
+                        InstalledVersion = $installedModule.Version
+                        LatestVersion = $latestModule.Version
+                    })
+                } else {
+                    $results.Add([PSCustomObject]@{
+                        ModuleName = $ModuleName
+                        Status = "Up-to-date"
+                        InstalledVersion = $installedModule.Version
+                        LatestVersion = $installedModule.Version
+                    })
+                }
+            } elseif (-not $installedModule) {
+                $results.Add([PSCustomObject]@{
+                    ModuleName = $ModuleName
+                    Status = "Not Installed"
+                    InstalledVersion = $null
+                    LatestVersion = $null
+                })
+            } else {
+                $results.Add([PSCustomObject]@{
+                    ModuleName = $ModuleName
+                    Status = "Not Found in Gallery"
+                    InstalledVersion = $null
+                    LatestVersion = $null
+                })
+            }
+        } catch {
+            Write-Error "An error occurred checking module '$ModuleName': $_"
+        }
+    }
+
+    return $results
+}
 
 function Update-ModuleIfOldOrMissing {
     <#
@@ -343,85 +400,3 @@ function Update-ModuleIfOldOrMissing {
 
 Update-ModuleIfOldOrMissing -ModuleName 'PSFramework'
 Update-ModuleIfOldOrMissing -ModuleName 'EnhancedModuleStarterAO'
-
-# function Install-EnhancedModuleStarterAO {
-#     <#
-#     .SYNOPSIS
-#     Installs the 'enhancedmodulestarterAO' module if it's not already installed, ensuring it's done in PowerShell 5 with 'AllUsers' scope and force.
-
-#     .DESCRIPTION
-#     This function checks if the 'enhancedmodulestarterAO' module is installed. If not, it ensures the system is running PowerShell 5 to install it with the 'AllUsers' scope and force. If not in PowerShell 5, it launches a new PS5 session to install and import the module.
-
-#     .EXAMPLE
-#     Install-EnhancedModuleStarterAO
-#     Installs the module 'enhancedmodulestarterAO' if not installed and ensures it's running in PowerShell 5.
-#     #>
-
-#     [CmdletBinding()]
-#     param ()
-
-#     # Helper function to detect PowerShell version
-   
-
-#     Begin {
-
-#         Write-EnhancedModuleStarterLog -Message "Starting Install-EnhancedModuleStarterAO function" -Level "Notice"
-
-
-#         Reset-ModulePaths
-
-#         # Check if the module is already installed
-#         $module = Get-Module -ListAvailable -Name "enhancedmodulestarterAO"
-#         if ($module) {
-#             Write-EnhancedModuleStarterLog -Message "Module 'enhancedmodulestarterAO' is already installed." -Level "INFO"
-#             return
-#         }
-#         Write-EnhancedModuleStarterLog -Message "Module 'enhancedmodulestarterAO' is not installed." -Level "INFO"
-#     }
-
-#     Process {
-#         # Check if running in PowerShell 5
-#         if (-not (Is-RunningInPS5)) {
-#             Write-EnhancedModuleStarterLog -Message "Not running in PowerShell 5, launching PS5 to install module." -Level "INFO"
-
-#             # Get the path to PowerShell 5
-#             $PS5Path = "${env:SystemRoot}\System32\WindowsPowerShell\v1.0\powershell.exe"
-
-#             # Define the arguments for the Start-Process
-#             $arguments = @(
-#                 # '-NoExit'
-#                 '-NoProfile'
-#                 '-ExecutionPolicy'
-#                 'Bypass'
-#                 '-Command'
-#                 'Install-Module -Name ''enhancedmodulestarterAO'' -Scope AllUsers -Force; Import-Module ''enhancedmodulestarterAO''; Write-Host ''Module installed and imported in PS5'''
-#             )
-
-#             # Launch a new PowerShell 5 process to install the module
-#             Start-Process -FilePath $PS5Path -ArgumentList $arguments -Verb RunAs -Wait
-#             return
-#         }
-
-#         # Install and import the module in PowerShell 5
-#         try {
-#             Write-EnhancedModuleStarterLog -Message "Running in PowerShell 5, installing module." -Level "INFO"
-#             Install-Module -Name "enhancedmodulestarterAO" -Scope AllUsers -Force
-
-#             # Import the module after installation
-#             Import-Module "enhancedmodulestarterAO"
-#             Write-EnhancedModuleStarterLog -Message "Module 'enhancedmodulestarterAO' installed and imported." -Level "INFO"
-#         }
-#         catch {
-#             Write-EnhancedModuleStarterLog -Message "Error installing or importing module: $($_.Exception.Message)" -Level "ERROR"
-#             # Handle-Error -ErrorRecord $_
-#             throw
-#         }
-#     }
-
-#     End {
-#         Write-EnhancedModuleStarterLog -Message "Exiting Install-EnhancedModuleStarterAO function" -Level "Notice"
-#     }
-# }
-
-# # Example usage
-# Install-EnhancedModuleStarterAO
