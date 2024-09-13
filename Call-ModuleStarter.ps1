a9db73e9529129781cf6421f28ec6407106bc5ee
aollivierre <abdullahollivierre@gmail.com>
2024-09-13 10:37:22 -0700

    archiving old module starter

--- Call-ModuleStarter.ps1
+++ Call-ModuleStarter.ps1
@@ -7,59 +7,30 @@
 # iex ((irm "https://raw.githubusercontent.com/aollivierre/module-starter/main/Module-Starter.ps1") -replace '\$Mode = "dev"', '$Mode = "dev"' -replace 'SkipPSGalleryModules\s*=\s*false', 'SkipPSGalleryModules = false' -replace '\$SkipCheckandElevate = \$false', '$SkipCheckandElevate = $true')
 
 
-iex (irm 'https://raw.githubusercontent.com/aollivierre/module-starter/main/Clone-EnhancedRepos.ps1')
+# iex (irm 'https://raw.githubusercontent.com/aollivierre/module-starter/main/Clone-EnhancedRepos.ps1')
 
 
-# Fetch the script content
-$scriptContent = Invoke-RestMethod "https://raw.githubusercontent.com/aollivierre/module-starter/main/Module-Starter.ps1"
+# # Fetch the script content
+# $scriptContent = Invoke-RestMethod "https://raw.githubusercontent.com/aollivierre/module-starter/main/Module-Starter.ps1"
 
-# Define replacements in a hashtable
-$replacements = @{
-    '\$Mode = "dev"'                     = '$Mode = "dev"'
-    '\$SkipPSGalleryModules = \$false'   = '$SkipPSGalleryModules = $true'
-    '\$SkipCheckandElevate = \$false'    = '$SkipCheckandElevate = $true'
-    '\$SkipAdminCheck = \$false'         = '$SkipAdminCheck = $true'
-    '\$SkipPowerShell7Install = \$false' = '$SkipPowerShell7Install = $true'
-    '\$SkipModuleDownload = \$false'     = '$SkipModuleDownload = $true'
-    '\$SkipGitrepos = \$false'           = '$SkipGitrepos = $true'
-}
+# # Define replacements in a hashtable
+# $replacements = @{
+#     '\$Mode = "dev"'                     = '$Mode = "dev"'
+#     '\$SkipPSGalleryModules = \$false'   = '$SkipPSGalleryModules = $true'
+#     '\$SkipCheckandElevate = \$false'    = '$SkipCheckandElevate = $true'
+#     '\$SkipAdminCheck = \$false'         = '$SkipAdminCheck = $true'
+#     '\$SkipPowerShell7Install = \$false' = '$SkipPowerShell7Install = $true'
+#     '\$SkipModuleDownload = \$false'     = '$SkipModuleDownload = $true'
+#     '\$SkipGitrepos = \$false'           = '$SkipGitrepos = $true'
+# }
 
-# Apply the replacements
-foreach ($pattern in $replacements.Keys) {
-    $scriptContent = $scriptContent -replace $pattern, $replacements[$pattern]
-}
+# # Apply the replacements
+# foreach ($pattern in $replacements.Keys) {
+#     $scriptContent = $scriptContent -replace $pattern, $replacements[$pattern]
+# }
 
-# Execute the script
-Invoke-Expression $scriptContent
-
-
-
-
-
-
-
-
-# Fetch the script content
-$scriptContent = Invoke-RestMethod "https://raw.githubusercontent.com/aollivierre/module-starter/main/Module-Starter.ps1"
-
-# Define replacements in a hashtable
-$replacements = @{
-    '\$Mode = "dev"'                     = '$Mode = "prod"'
-    '\$SkipPSGalleryModules = \$false'   = '$SkipPSGalleryModules = $false'
-    '\$SkipCheckandElevate = \$false'    = '$SkipCheckandElevate = $false'
-    '\$SkipAdminCheck = \$false'         = '$SkipAdminCheck = $false'
-    '\$SkipPowerShell7Install = \$false' = '$SkipPowerShell7Install = $false'
-    '\$SkipModuleDownload = \$false'     = '$SkipModuleDownload = $false'
-    '\$SkipGitrepos = \$false'           = '$SkipGitrepos = $false'
-}
-
-# Apply the replacements
-foreach ($pattern in $replacements.Keys) {
-    $scriptContent = $scriptContent -replace $pattern, $replacements[$pattern]
-}
-
-# Execute the script
-Invoke-Expression $scriptContent
+# # Execute the script
+# Invoke-Expression $scriptContent
 
 
 
@@ -68,6 +39,27 @@ Invoke-Expression $scriptContent
 
 
 
+# # Fetch the script content
+# $scriptContent = Invoke-RestMethod "https://raw.githubusercontent.com/aollivierre/module-starter/main/Module-Starter.ps1"
+
+# # Define replacements in a hashtable
+# $replacements = @{
+#     '\$Mode = "dev"'                     = '$Mode = "prod"'
+#     '\$SkipPSGalleryModules = \$false'   = '$SkipPSGalleryModules = $false'
+#     '\$SkipCheckandElevate = \$false'    = '$SkipCheckandElevate = $false'
+#     '\$SkipAdminCheck = \$false'         = '$SkipAdminCheck = $false'
+#     '\$SkipPowerShell7Install = \$false' = '$SkipPowerShell7Install = $false'
+#     '\$SkipModuleDownload = \$false'     = '$SkipModuleDownload = $false'
+#     '\$SkipGitrepos = \$false'           = '$SkipGitrepos = $false'
+# }
+
+# # Apply the replacements
+# foreach ($pattern in $replacements.Keys) {
+#     $scriptContent = $scriptContent -replace $pattern, $replacements[$pattern]
+# }
+
+# # Execute the script
+# Invoke-Expression $scriptContent
 
 
 # Set environment variable globally for all users
--- Module-Starter-ALPHAv1.ps1
+++ Module-Starter-ALPHAv1.ps1
@@ -1,24 +1,24 @@
 # Fetch the script content
-$scriptContent = Invoke-RestMethod "https://raw.githubusercontent.com/aollivierre/module-starter/main/Module-Starter.ps1"
+# $scriptContent = Invoke-RestMethod "https://raw.githubusercontent.com/aollivierre/module-starter/main/Module-Starter.ps1"
 
-# Define replacements in a hashtable
-$replacements = @{
-    '\$Mode = "dev"'                     = '$Mode = "dev"'
-    '\$SkipPSGalleryModules = \$false'   = '$SkipPSGalleryModules = $true'
-    '\$SkipCheckandElevate = \$false'    = '$SkipCheckandElevate = $true'
-    '\$SkipAdminCheck = \$false'         = '$SkipAdminCheck = $true'
-    '\$SkipPowerShell7Install = \$false' = '$SkipPowerShell7Install = $true'
-    '\$SkipModuleDownload = \$false'     = '$SkipModuleDownload = $true'
-    '\$SkipGitrepos = \$false'           = '$SkipGitrepos = $true'
-}
+# # Define replacements in a hashtable
+# $replacements = @{
+#     '\$Mode = "dev"'                     = '$Mode = "dev"'
+#     '\$SkipPSGalleryModules = \$false'   = '$SkipPSGalleryModules = $true'
+#     '\$SkipCheckandElevate = \$false'    = '$SkipCheckandElevate = $true'
+#     '\$SkipAdminCheck = \$false'         = '$SkipAdminCheck = $true'
+#     '\$SkipPowerShell7Install = \$false' = '$SkipPowerShell7Install = $true'
+#     '\$SkipModuleDownload = \$false'     = '$SkipModuleDownload = $true'
+#     '\$SkipGitrepos = \$false'           = '$SkipGitrepos = $true'
+# }
 
-# Apply the replacements
-foreach ($pattern in $replacements.Keys) {
-    $scriptContent = $scriptContent -replace $pattern, $replacements[$pattern]
-}
+# # Apply the replacements
+# foreach ($pattern in $replacements.Keys) {
+#     $scriptContent = $scriptContent -replace $pattern, $replacements[$pattern]
+# }
 
-# Execute the script
-Invoke-Expression $scriptContent
+# # Execute the script
+# Invoke-Expression $scriptContent
 
 
 
