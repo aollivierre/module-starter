260e901a5f0038b76b4cbc9938a0ae92425c09ec
aollivierre <abdullahollivierre@gmail.com>
2024-09-13 09:10:05 -0700

    adding NuGet provider function

--- Install-EnhancedModuleStarterAO.ps1
+++ Install-EnhancedModuleStarterAO.ps1
@@ -444,6 +444,31 @@ function Install-ModuleInPS5 {
     }
 }
 
+
+function Ensure-NuGetProvider {
+    # Ensure NuGet provider and PowerShellGet module are installed if running in PowerShell 5
+    if ($PSVersionTable.PSVersion.Major -eq 5) {
+        if (-not (Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue)) {
+            Install-PackageProvider -Name NuGet -Force -Confirm:$false
+            # Install-Module -Name PowerShellGet -Scope AllUsers -Force -AllowClobber
+
+            $params = @{
+                ModuleName = "PowerShellGet"
+            }
+            Install-ModuleInPS5 @params
+
+            Write-EnhancedModuleStarterLog -Message "NuGet provider installed successfully." -Level "INFO"
+        }
+        else {
+            Write-EnhancedModuleStarterLog -Message "NuGet provider is already installed." -Level "INFO"
+        }
+    }
+    else {
+        Write-EnhancedModuleStarterLog -Message "This script is running in PowerShell version $($PSVersionTable.PSVersion) which is not version 5. No action is taken for NuGet" -Level "INFO"
+    }
+}
+
+
 function Check-ModuleVersionStatus {
     <#
     .SYNOPSIS
