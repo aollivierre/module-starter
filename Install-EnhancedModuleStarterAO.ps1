ee91af80c469cb92004b54688594f226568bed9c
aollivierre <abdullahollivierre@gmail.com>
2024-09-16 12:18:22 -0700

    commenting out redundant functions

--- Install-EnhancedModuleStarterAO.ps1
+++ Install-EnhancedModuleStarterAO.ps1
@@ -388,53 +388,53 @@ function Log-Params {
     }
 }
 
-function Reset-ModulePaths {
-    [CmdletBinding()]
-    param ()
+# function Reset-ModulePaths {
+#     [CmdletBinding()]
+#     param ()
 
-    begin {
-        # Initialization block, typically used for setup tasks
-        Write-EnhancedModuleStarterLog -Message "Initializing Reset-ModulePaths function..." -Level "DEBUG"
-    }
+#     begin {
+#         # Initialization block, typically used for setup tasks
+#         Write-EnhancedModuleStarterLog -Message "Initializing Reset-ModulePaths function..." -Level "DEBUG"
+#     }
 
-    process {
-        try {
-            # Log the start of the process
-            Write-EnhancedModuleStarterLog -Message "Resetting module paths to default values..." -Level "INFO"
+#     process {
+#         try {
+#             # Log the start of the process
+#             Write-EnhancedModuleStarterLog -Message "Resetting module paths to default values..." -Level "INFO"
 
-            # Get the current user's Documents path
-            $userModulesPath = [System.IO.Path]::Combine($env:USERPROFILE, 'Documents\WindowsPowerShell\Modules')
+#             # Get the current user's Documents path
+#             $userModulesPath = [System.IO.Path]::Combine($env:USERPROFILE, 'Documents\WindowsPowerShell\Modules')
 
-            # Define the default module paths
-            $defaultModulePaths = @(
-                "C:\Program Files\WindowsPowerShell\Modules",
-                $userModulesPath,
-                "C:\Windows\System32\WindowsPowerShell\v1.0\Modules"
-            )
+#             # Define the default module paths
+#             $defaultModulePaths = @(
+#                 "C:\Program Files\WindowsPowerShell\Modules",
+#                 $userModulesPath,
+#                 "C:\Windows\System32\WindowsPowerShell\v1.0\Modules"
+#             )
 
-            # Attempt to reset the PSModulePath environment variable
-            $env:PSModulePath = [string]::Join(';', $defaultModulePaths)
-            Write-EnhancedModuleStarterLog "PSModulePath successfully set to: $($env:PSModulePath -split ';' | Out-String)" -Level "INFO"
+#             # Attempt to reset the PSModulePath environment variable
+#             $env:PSModulePath = [string]::Join(';', $defaultModulePaths)
+#             Write-EnhancedModuleStarterLog "PSModulePath successfully set to: $($env:PSModulePath -split ';' | Out-String)" -Level "INFO"
 
-            # Optionally persist the change for the current user
-            [Environment]::SetEnvironmentVariable("PSModulePath", $env:PSModulePath, [EnvironmentVariableTarget]::User)
-            Write-EnhancedModuleStarterLog -Message "PSModulePath environment variable set for the current user." -Level "INFO"
-        }
-        catch {
-            # Capture and log any errors that occur during the process
-            $errorMessage = $_.Exception.Message
-            Write-EnhancedModuleStarterLog -Message "Error resetting module paths: $errorMessage" -Level "ERROR"
+#             # Optionally persist the change for the current user
+#             [Environment]::SetEnvironmentVariable("PSModulePath", $env:PSModulePath, [EnvironmentVariableTarget]::User)
+#             Write-EnhancedModuleStarterLog -Message "PSModulePath environment variable set for the current user." -Level "INFO"
+#         }
+#         catch {
+#             # Capture and log any errors that occur during the process
+#             $errorMessage = $_.Exception.Message
+#             Write-EnhancedModuleStarterLog -Message "Error resetting module paths: $errorMessage" -Level "ERROR"
 
-            # Optionally, you could throw the error to halt the script
-            throw $_
-        }
-    }
+#             # Optionally, you could throw the error to halt the script
+#             throw $_
+#         }
+#     }
 
-    end {
-        # Finalization block, typically used for cleanup tasks
-        Write-EnhancedModuleStarterLog -Message "Reset-ModulePaths function completed." -Level "DEBUG"
-    }
-}
+#     end {
+#         # Finalization block, typically used for cleanup tasks
+#         Write-EnhancedModuleStarterLog -Message "Reset-ModulePaths function completed." -Level "DEBUG"
+#     }
+# }
 
 function Handle-Error {
     param (
