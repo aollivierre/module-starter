1f3c43eb475a0c112381f135c4618d2b056d0358
aollivierre <abdullahollivierre@gmail.com>
2024-09-17 09:47:20 -0700

    providing caller examples for both prod and dev

--- Call-ModuleStarter-prod.ps1
+++ Call-ModuleStarter-prod.ps1
@@ -112,3 +112,79 @@ switch ($mode) {
         # Default to production
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Set environment variable globally for all users
+[System.Environment]::SetEnvironmentVariable('EnvironmentMode', 'dev', 'Machine')
+
+# Retrieve the environment mode (default to 'prod' if not set)
+$mode = $env:EnvironmentMode
+
+#region FIRING UP MODULE STARTER
+#################################################################################################
+#                                                                                               #
+#                                 FIRING UP MODULE STARTER                                      #
+#                                                                                               #
+#################################################################################################
+
+Invoke-Expression (Invoke-RestMethod "https://raw.githubusercontent.com/aollivierre/module-starter/main/Install-EnhancedModuleStarterAO.ps1")
+
+# Wait-Debugger
+
+# Define a hashtable for splatting
+$moduleStarterParams = @{
+    Mode                   = 'dev'
+    SkipPSGalleryModules   = $false
+    SkipCheckandElevate    = $false
+    SkipPowerShell7Install = $false
+    SkipEnhancedModules    = $false
+    SkipGitRepos           = $false
+}
+
+# Call the function using the splat
+Invoke-ModuleStarter @moduleStarterParams
+
+
+# Wait-Debugger
+
+#endregion FIRING UP MODULE STARTER
+
+# Toggle based on the environment mode
+switch ($mode) {
+    'dev' {
+        Write-EnhancedLog -Message "Running in development mode" -Level 'WARNING'
+        # Your development logic here
+    }
+    'prod' {
+        Write-EnhancedLog -Message "Running in production mode" -ForegroundColor Green
+        # Your production logic here
+    }
+    default {
+        Write-EnhancedLog -Message "Unknown mode. Defaulting to production." -ForegroundColor Red
+        # Default to production
+    }
+}
