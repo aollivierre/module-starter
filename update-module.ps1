6d5f8ed43b1fb5fdd96be79d661eeb5d2d30e8f0
aollivierre <abdullahollivierre@gmail.com>
2024-09-17 13:21:38 -0700

    Update update-module.ps1

--- update-module.ps1
+++ update-module.ps1
@@ -2,21 +2,46 @@ param (
     [string]$ModuleName
 )
 
+# try {
+#     Update-ModuleIfOldOrMissing -ModuleName $ModuleName
+#     $moduleInfo = Get-Module -Name $ModuleName -ListAvailable | Sort-Object Version -Descending | Select-Object -First 1
+#     $moduleDetails = [pscustomobject]@{
+#         Name    = $ModuleName
+#         Version = $moduleInfo.Version
+#         Path    = $moduleInfo.ModuleBase
+#     }
+#     Write-EnhancedLog -Message "Success: $(ConvertTo-Json $moduleDetails)"
+# }
+# catch {
+#     $moduleDetails = [pscustomobject]@{
+#         Name    = $ModuleName
+#         Version = "N/A"
+#         Path    = "N/A"
+#     }
+#     Write-EnhancedLog -Message "Failure: $(ConvertTo-Json $moduleDetails)"
+# }
+
+
+
 try {
-    Update-ModuleIfOldOrMissing -ModuleName $ModuleName
-    $moduleInfo = Get-Module -Name $ModuleName -ListAvailable | Sort-Object Version -Descending | Select-Object -First 1
-    $moduleDetails = [pscustomobject]@{
-        Name    = $ModuleName
+    Update-ModuleIfOldOrMissing -ModuleName $moduleName
+    $moduleInfo = Get-Module -Name $moduleName -ListAvailable | Sort-Object Version -Descending | Select-Object -First 1
+    $moduleDetails = [PSCustomObject]@{
+        Name    = $moduleName
         Version = $moduleInfo.Version
         Path    = $moduleInfo.ModuleBase
     }
-    Write-Output "Success: $(ConvertTo-Json $moduleDetails)"
+    $successModules.Add($moduleDetails)
+    Write-EnhancedLog -Message "Successfully installed/updated module: $moduleName" -Level "INFO"
+    $moduleSuccessCount++
 }
 catch {
-    $moduleDetails = [pscustomobject]@{
-        Name    = $ModuleName
+    $moduleDetails = [PSCustomObject]@{
+        Name    = $moduleName
         Version = "N/A"
         Path    = "N/A"
     }
-    Write-Output "Failure: $(ConvertTo-Json $moduleDetails)"
+    $failedModules.Add($moduleDetails)
+    Write-EnhancedLog -Message "Failed to install/update module: $moduleName. Error: $_" -Level "ERROR"
+    $moduleFailCount++
 }
