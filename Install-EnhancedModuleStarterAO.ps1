0395263201adb7fb7b1834e2b8bc1271e202881b
aollivierre <abdullahollivierre@gmail.com>
2024-09-28 17:30:25 -0700

    fixing function order for script name

--- Install-EnhancedModuleStarterAO.ps1
+++ Install-EnhancedModuleStarterAO.ps1
@@ -9,6 +9,38 @@ else {
     Set-Variable -Name 'SimulatingIntune' -Value $false
 }
 
+function Get-ParentScriptName {
+    [CmdletBinding()]
+    param ()
+
+    try {
+        # Get the current call stack
+        $callStack = Get-PSCallStack
+
+        # If there is a call stack, return the top-most script name
+        if ($callStack.Count -gt 0) {
+            foreach ($frame in $callStack) {
+                if ($frame.ScriptName) {
+                    $parentScriptName = $frame.ScriptName
+                    # Write-EnhancedModuleStarterLog -Message "Found script in call stack: $parentScriptName" -Level "INFO"
+                }
+            }
+
+            if (-not [string]::IsNullOrEmpty($parentScriptName)) {
+                $parentScriptName = [System.IO.Path]::GetFileNameWithoutExtension($parentScriptName)
+                return $parentScriptName
+            }
+        }
+
+        # If no script name was found, return 'UnknownScript'
+        # Write-EnhancedModuleStarterLog -Message "No script name found in the call stack." -Level "WARNING"
+        return "UnknownScript"
+    }
+    catch {
+        # Write-EnhancedModuleStarterLog -Message "An error occurred while retrieving the parent script name: $_" -Level "ERROR"
+        return "UnknownScript"
+    }
+}
 
 function Write-EnhancedModuleStarterLog {
     param (
@@ -402,38 +434,7 @@ function CheckAndElevate {
     }
 }
 
-function Get-ParentScriptName {
-    [CmdletBinding()]
-    param ()
 
-    try {
-        # Get the current call stack
-        $callStack = Get-PSCallStack
-
-        # If there is a call stack, return the top-most script name
-        if ($callStack.Count -gt 0) {
-            foreach ($frame in $callStack) {
-                if ($frame.ScriptName) {
-                    $parentScriptName = $frame.ScriptName
-                    # Write-EnhancedModuleStarterLog -Message "Found script in call stack: $parentScriptName" -Level "INFO"
-                }
-            }
-
-            if (-not [string]::IsNullOrEmpty($parentScriptName)) {
-                $parentScriptName = [System.IO.Path]::GetFileNameWithoutExtension($parentScriptName)
-                return $parentScriptName
-            }
-        }
-
-        # If no script name was found, return 'UnknownScript'
-        # Write-EnhancedModuleStarterLog -Message "No script name found in the call stack." -Level "WARNING"
-        return "UnknownScript"
-    }
-    catch {
-        # Write-EnhancedModuleStarterLog -Message "An error occurred while retrieving the parent script name: $_" -Level "ERROR"
-        return "UnknownScript"
-    }
-}
 
 function Log-Params {
     <#
